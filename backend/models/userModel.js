c4439be22a0f3b06c50bb09c9e405daa6299ddd4
RichGod <usenrichgod93@gmail.com>
2022-12-09 09:42:56 +0100

    signup logic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
 });
 
 // static signup method
-userSchema.statics.signup = async (email, password) => {
+userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email });
 
   if (exists) {
