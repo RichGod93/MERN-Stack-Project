7934835a342b658fb069f4753c8b8b5d2b542b70
RichGod <usenrichgod93@gmail.com>
2022-12-09 19:20:44 +0100

    debugging errors

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const requireAuth = async (req, res, next) => {
-  // verify authentication
+  // verify user is authenticated
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.satus(401).json({ error: "Authorization token required" });
+    return res.status(401).json({ error: "Authorization token required" });
   }
 
   const token = authorization.split(" ")[1];
@@ -21,3 +21,5 @@ const requireAuth = async (req, res, next) => {
     res.status(401).json({ error: "Request is not authorized" });
   }
 };
+
+module.exports = requireAuth;
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,22 +6,26 @@ const {
   deleteWorkout,
   updateWorkout,
 } = require("../controllers/workoutControllers");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth);
 
 // GET all workouts
 router.get("/", getWorkouts);
 
-// GET a single workout
+//GET a single workout
 router.get("/:id", getWorkout);
 
 // POST a new workout
 router.post("/", createWorkout);
 
-// DELETE a new workout
+// DELETE a workout
 router.delete("/:id", deleteWorkout);
 
-// UPDATE a new workout
+// UPDATE a workout
 router.patch("/:id", updateWorkout);
 
 module.exports = router;
